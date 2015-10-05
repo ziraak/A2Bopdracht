fd4494535564f023e35dbe9e52c03a628396235a
ziraak <harmenjwiersma@hotmail.com>
2015-10-05 11:25:10 +0200

    added prototypes for main class

--- src/main/java/A2B.java
+++ src/main/java/A2B.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+
 /**
  * Created by Harmen on 5-10-2015.
  * Remember that Harmen is awesome
  */
 public class A2B {
 
+    public void getRouteInfo(String beginpunt, String eindpunt){
+
+    }
+     private ArrayList checkDuplicateVerkeersinfo(ArrayList verkeersinfo){
+         return null;
+     }
 }
