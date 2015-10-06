f282ffe0a2bf4a0f342d0072aeae673fc7a0c9a1
ziraak <harmenjwiersma@hotmail.com>
2015-10-06 12:39:36 +0200

    todo weggehaald

--- src/main/java/Belemmering.java
+++ src/main/java/Belemmering.java
@@ -3,7 +3,6 @@
  */
 public class Belemmering extends VerkeersInfo implements DynamischeGegevens {
     public int vertraging;
-//TODO CONSTRUCTOR
 
     public int getVertraging() {
         return vertraging;
